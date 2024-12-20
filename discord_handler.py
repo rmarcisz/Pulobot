5c9d1aef5bc2d5461a4d2037834021fab324152b
Radomir Marcisz <raderkpm@gmail.com>
2024-12-20 12:28:17 +0100

    cleaned code, added more options, bot will now inform about bad inputs, created a anti infinity loop mechanisms

--- discord_handler.py
+++ discord_handler.py
@@ -17,9 +17,12 @@ bot = discord.Client(intents=intents)
 async def on_message(message):
     scenario_images = []
     if message.content[:5] == '!pule':
-        files = pulobot.get_pule(str(message.content[5:]))
-        for file in files:
-            scenario_images.append(discord.File(file))
-        await message.channel.send(files=scenario_images)
+        if pulobot.message_read(message.content[5:]) == 'error':
+            await message.channel.send('Unrecognized input. Please format the message as !pule [number of rounds],[gg],[name],[options]. \n For more information please check https://wincyjneverbornow.blogspot.com/2024/12/pulobotmanual.html')
+        else:
+            files = pulobot.get_pule(str(message.content[6:]))
+            for file in files:
+                scenario_images.append(discord.File(file))
+            await message.channel.send(files=scenario_images)
 
 bot.run(DISCORD_TOKEN)
--- pulobot.py
+++ pulobot.py
@@ -13,9 +13,10 @@ def message_read(message):
     try:
         rounds_number = min(int(arguments[0].strip()), 10)
     except:
-        rounds_number = 'Invalid message. Message should be formatted as [number of rounds, gg to be used, event name, parameters] to work. Please refer to user manual at wincyjneverbornów.blogspot'
+        return 'error'
     try:
         gg_input = arguments[1].strip()
+        gg_input = gg_input.lower()
     except:
         gg_input = 'gg4'
     try:
@@ -26,8 +27,7 @@ def message_read(message):
         options = arguments[3].strip()
     except:
         options = ''
-    message_author = 'Unnamed user'
-    return [rounds_number, gg_input, event_name, options, request_time, message_author]
+    return [rounds_number, gg_input, event_name, options, request_time]
 
 def read_gg(gg_input):
     scenario_gg = {'Primary': [], 'Secondary': []}
@@ -63,35 +63,50 @@ def strat_generator(rounds_number, scenario_gg):
     return strat_pool
 
 def scheme_generator(rounds_number, scenario_gg):
-    scheme_pool = []
     pool_of_scheme_pools = []
-    primary_schemes = []
-    secondary_schemes = []
-    sanity_passed = False
-    while sanity_passed == False:
-        while len(scheme_pool) < rounds_number * 5:
-            if primary_schemes != []:
-                scheme_pool.append(primary_schemes.pop(random.choice(range(len(primary_schemes)))))
-            elif secondary_schemes != []:
-                scheme_pool.append(secondary_schemes.pop(random.choice(range(len(secondary_schemes)))))
+    empty_pool = []
+    def scheme_pool_randomizer():
+        scheme_pool = []
+        primary_schemes = []
+        secondary_schemes = []
+        sanity_passed = False
+        while sanity_passed == False:
+            while len(scheme_pool) < rounds_number * 5:
+                if primary_schemes != []:
+                    scheme_pool.append(primary_schemes.pop(random.choice(range(len(primary_schemes)))))
+                elif secondary_schemes != []:
+                    scheme_pool.append(secondary_schemes.pop(random.choice(range(len(secondary_schemes)))))
+                else:
+                    primary_schemes = refill(scenario_gg['Primary'], 'schemes')
+                    secondary_schemes = refill(scenario_gg['Secondary'], 'schemes')
+            if scheme_pool.count(max(set(scheme_pool), key=scheme_pool.count)) <= rounds_number:
+                sanity_passed = True
             else:
-                primary_schemes = refill(scenario_gg['Primary'], 'schemes')
-                secondary_schemes = refill(scenario_gg['Secondary'], 'schemes')
-        if scheme_pool.count(max(set(scheme_pool), key=scheme_pool.count)) <= rounds_number:
-            sanity_passed = True
-        else:
-            scheme_pool = []
+                scheme_pool = []
+        return scheme_pool
+    scheme_pool = scheme_pool_randomizer()
     for round in range(rounds_number):
         pool_of_scheme_pools.append([])
+    print(pool_of_scheme_pools)
     while scheme_pool != []:
+        i = 0
         scheme = max(set(scheme_pool), key=scheme_pool.count)
         while scheme in scheme_pool:
-            updated = random.choice(pool_of_scheme_pools)
-            if len(updated) < 5:
-                if scheme not in updated:
-                    updated.append(scheme)
-                    scheme_pool.remove(scheme)
+            if i > 100:
+                pool_of_scheme_pools = []
+                for round in range(rounds_number):
+                    pool_of_scheme_pools.append([])
+                scheme_pool = scheme_pool_randomizer()
+                break
+            else:
+                i += 1
+                updated = random.choice(pool_of_scheme_pools)
+                if len(updated) < 5:
+                    if scheme not in updated:
+                        updated.append(scheme)
+                        scheme_pool.remove(scheme)
     return pool_of_scheme_pools
+
 
 def scenario_generator(rounds_number, strat_pool, pool_of_scheme_pools):
     scenarios = []
@@ -106,7 +121,17 @@ def scenario_generator(rounds_number, strat_pool, pool_of_scheme_pools):
         scenario['Schemes'] = pool_of_scheme_pools.pop(random.choice(range(len(pool_of_scheme_pools))))
     return scenarios
 
-def qrcode_generator(scenarios, event_name,request_time,message_author, options):
+def qrcode_generator(scenarios, event_name,options,request_time):
+    rules = {}
+    if 'sin' in options:
+        rules['Singles'] = {'name': 'Singles'}
+    if '-ban' in options:
+        if '-ban3' in options:
+            rules['Bans'] = {'name': 'Bans', 'value':3}
+        elif '-ban2' in options:
+            rules['Bans'] = {'name': 'Bans', 'value':2}
+        else:
+            rules['Bans'] = {'name': 'Bans', 'value':1}
     images_names = []
     path = f'''output/{event_name}_{request_time}'''
     os.mkdir(path)
@@ -114,17 +139,11 @@ def qrcode_generator(scenarios, event_name,request_time,message_author, options)
         schemes_decoded = []
         for scheme in scenario['Schemes']:
             schemes_decoded.append(all_gg['ggall']['schemes'].index(scheme))
-        data_input = {"specialRules":{},"name": event_name + ' Round ' + str(scenario['Round']), "ruleset":"All", "strat": scenario['Strategy'], "deployment": scenario['Deployment'], "maxCrewSize": 50, "createdIn":"1.7.24", "created": "2004-04-02T21:37:00.000", "schemePool": schemes_decoded}
+        data_input = {"specialRules":rules,"name": event_name + ' Round ' + str(scenario['Round']), "ruleset":"All", "strat": scenario['Strategy'], "deployment": scenario['Deployment'], "maxCrewSize": 50, "createdIn":"1.7.24", "created": "2004-04-02T21:37:00.000", "schemePool": schemes_decoded}
         imago = qrcode.make(json.dumps(data_input))
         imago = imago.resize((690, 690))
         imago.save(f'{path}/qr.png')
-        image1 = Image.new("RGB", (1500, 750), color=(47,49,54))
-        if '-vas' in options:
-            image1 = Image.new("RGB", (2250, 750), color=(47,49,54))
-            map_num = random.choice(range(1, len(vassal_maps)))
-            vassal_map = Image.open(f'vassal_maps/{map_num}.png')
-            image1.paste(vassal_map, (1530, 30))
-            vassal_map.close
+        image1 = Image.new('RGB', (1500, 750), color=(47,49,54))
         image2 = Image.open(f'{path}/qr.png')
         image1.paste(image2, (30, 30))
         image2.close
@@ -140,23 +159,36 @@ def qrcode_generator(scenarios, event_name,request_time,message_author, options)
         w = draw.textlength(f'''Strategy: {scenario['Strategy']}''', font=fnt)
         draw.text(xy=(1125-(w/2), 250), text=f'''Strategy: {scenario['Strategy']}''', font=fnt, fill=(255, 255, 255), stroke_width=5, stroke_fill=(0,0,0))
         w = draw.textlength('Schemes:', font=fnt)
-        draw.text(xy=(1125-(w/2), 300), text='Schemes:', font=fnt, fill=(255, 255, 255), stroke_width=5, stroke_fill=(0,0,0))
-        if '-vas' in options:
-            w = draw.textlength(f'[{map_num}] {vassal_maps[map_num]}', font=fnt)
-            draw.text(xy=(1875-(w/2), 50), text=f'[{map_num}] {vassal_maps[map_num]}', font=fnt, fill=(255, 255, 255), stroke_width=5, stroke_fill=(0,0,0))
+        draw.text(xy=(1125-(w/2), 300), text='Schemes:', font=fnt, fill=(255, 255, 255), stroke_width=5, stroke_fill=(0,0,0))    
         for num, scheme in enumerate(scenario['Schemes']):
             w = draw.textlength(f'''{scheme}''', font=fnt)
             y = 350 + num*50
             draw.text(xy=(1125-(w/2), y), text=f'''{scheme}''', font=fnt, fill=(255, 255, 255), stroke_width=5, stroke_fill=(0,0,0))
+        if '-vas' in options:
+            image_vas = Image.new('RGB', (760, 750), color=(47,49,54))
+            map_num = random.choice(range(1, len(vassal_maps)))
+            vassal_map = Image.open(f'vassal_maps/{map_num}.png')
+            image_vas.paste(vassal_map, (30, 30))
+            vassal_map.close
+            draw = ImageDraw.Draw(image_vas)
+            w = draw.textlength(f'[{map_num}] {vassal_maps[map_num]}', font=fnt)
+            draw.text(xy=(375-(w/2), 50), text=f'[{map_num}] {vassal_maps[map_num]}', font=fnt, fill=(255, 255, 255), stroke_width=5, stroke_fill=(0,0,0))
+            new_image = Image.new('RGB', (image1.size[0] + image_vas.size[0], 750), color=(47,49,54))
+            new_image.paste(image1, (0,0))
+            new_image.paste(image_vas, (1500,0))
+            image1 = new_image
         image1.save(f'''{path}/{scenario['Round']}.png''')
         images_names.append(f'''{path}/{scenario['Round']}.png''')
     return images_names
 
 def get_pule(message):
     user_input = message_read(message)
-    gg_input = read_gg(user_input[1])
-    strat_generated = strat_generator(user_input[0], gg_input)
-    schemes_generated = scheme_generator(user_input[0], gg_input)
-    scenarios = scenario_generator(user_input[0], strat_generated, schemes_generated)
-    qr_codes = qrcode_generator(scenarios, user_input[2],user_input[4],user_input[5], user_input[3])
-    return qr_codes
+    if user_input == 'error':
+        return 'error'
+    else:
+        gg_input = read_gg(user_input[1])
+        strat_generated = strat_generator(user_input[0], gg_input)
+        schemes_generated = scheme_generator(user_input[0], gg_input)
+        scenarios = scenario_generator(user_input[0], strat_generated, schemes_generated)
+        qr_codes = qrcode_generator(scenarios, user_input[2],user_input[3],user_input[4])
+        return qr_codes
